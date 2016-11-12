af61167b3c310542fe12e0d2a39be219eade5531
Siyuan Qiao <joe.siyuan.qiao@gmail.com>
2016-11-11 22:45:59 -0500

    [TrainingStage] add game resume

--- Source/UnrealCV/Private/Commands/ActionHandler.cpp
+++ Source/UnrealCV/Private/Commands/ActionHandler.cpp
@@ -10,16 +10,29 @@ void FActionCommandHandler::RegisterCommands()
 	Help = "Pause the game";
 	CommandDispatcher->BindCommand("vset /action/game/pause", Cmd, Help);
 
-	/*
 	Cmd = FDispatcherDelegate::CreateRaw(this, &FActionCommandHandler::ResumeGame);
 	Help = "Resume the game";
 	CommandDispatcher->BindCommand("vset /action/game/resume", Cmd, Help);
-	*/
 }
 
 FExecStatus FActionCommandHandler::PauseGame(const TArray<FString>& Args)
 {
 	APlayerController* PlayerController = GWorld->GetFirstPlayerController();
-	PlayerController->Pause();
+	if (!PlayerController->IsPaused())
+	{
+		PlayerController->Pause();
+	}
 	return FExecStatus::OK();
 }
+
+FExecStatus FActionCommandHandler::ResumeGame(const TArray<FString>& Args)
+{
+	APlayerController* PlayerController = GWorld->GetFirstPlayerController();
+	if (PlayerController->IsPaused())
+	{
+		PlayerController->Pause();
+	}
+	return FExecStatus::OK();
+}
+
+
